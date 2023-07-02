42485ea7d7ee6a66482fe95b4eaf84bf42400023
Phanuphong Machai <phanuphong.forwork@gmail.com>
2023-07-02 11:50:15 +0700

    feat: new

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const { Client, GatewayIntentBits } = require("discord.js");
 const { createClient } = require("@supabase/supabase-js");
 require("dotenv").config();
 
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
+
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
 
